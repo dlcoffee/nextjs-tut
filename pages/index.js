87298e2dd8e536491dfbfb3fcd2a814bd03b1fe9
Daniel Tea <daniel.tea@onehopewine.com>
2018-05-14 13:31:05 -0700

    add scoped styling

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,45 @@ import fetch from 'isomorphic-unfetch';
 
 import Layout from '../components/Layout';
 
+const ShowLink = ({ show }) => (
+  <li>
+    <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+      <a>{show.name}</a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </li>
+);
+
 const Index = (props) => (
   <Layout>
     <h1>Batman TV Shows</h1>
     <ul>
-      { props.shows.map(({ show }) => (
-          <li key={show.id}>
-            <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-              <a>{show.name}</a>
-            </Link>
-          </li>)
-        )
-      }
+      { props.shows.map(({ show }) => <ShowLink key={show.id} show={show}/>) }
     </ul>
+    <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+    `}</style>
   </Layout>
 );
 
